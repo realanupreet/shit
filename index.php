fe48e6ef174e2b123569da20232eec5aa02250f2
realanupreet <72339688+realanupreet@users.noreply.github.com>
2022-01-18 08:21:58 +0530

    doing the php stuff

--- index.php
+++ index.php
@@ -18,21 +18,49 @@
     $num = 90;
     echo "Hello $num $charname";
 
-$phrase = "hdnDD giraffe DDnk";
-$age =60;
-$float= 6.63;
-echo $age; 
-echo "<hr>";
-echo strtolower($phrase[5]);
-echo strlen($phrase);
-echo $phrase;
-echo "<br>";
-$phrase = str_replace("giraffe","panda",$phrase);
-echo $phrase;
-
-
+    $phrase = "hdnDD giraffe DDnk";
+    $age = 60;
+    $float = 6.63;
+    echo $age;
+    echo "<hr>";
+    echo strtolower($phrase[5]);
+    echo strlen($phrase);
+    echo $phrase;
+    echo "<br>";
+    $phrase = str_replace("giraffe", "panda", $phrase);
+    echo $phrase;
+    echo "<br>";
+    echo substr($phrase, 5, 2);
+    echo min(sqrt(pow(4, 5)), 56);
+    $nahme = "";
     ?>
 
+
+    <form action="index.php" method="get">
+        Name: <input type="text" name="name" id="name">
+        <input type="submit">
+    </form>
+    <br>
+
+    <?php
+
+    $name = array("kevin", "barry");
+    echo $name[1];
+
+    $grades = array("shh" => "s", "ssd" => "df");
+    echo $grades["ssd"];
+
+
+    function sayHi($name)
+    {
+        echo " <hr>hello user $name <hr>";
+    }
+
+    sayHi("mickey");
+    ?>
+
+
+    Your name is <?php echo $_GET["name"] ?? null; ?>
 
 </body>
 
